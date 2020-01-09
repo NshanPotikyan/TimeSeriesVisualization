0d4c5170961d1fa8b9812152ea8e3bd4bde45700
Ubuntu <ubuntu@shota.cloud.ut.ee>
2020-01-09 16:06:51 +0000

    neat

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 #from helpers import create_plot
 from dtw import DTW
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'csv','tsv'}
 
 app = Flask(__name__,static_url_path='/images')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'file uploaded successfully'
+
 
 @app.route('/plot')
 def plot():
